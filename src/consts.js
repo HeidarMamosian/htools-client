49006aa605a3ed66065a26eda35556d6c22a47b9
Jake <jakeincanada@icloud.com>
2018-10-16 20:45:58 +0000

    restore previous getRelativeApi functionality

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 function getRelativeAPI(host, port, path, https) {
     const hostWithoutPort = host.replace(/:\d{1,6}\/?$/, "");
-    const httpStart = https == null ? "//" : https+ "//";
-    // const httpStart = https == null ? "//" : (https ? "https://" : "http://");
+    // const httpStart = https == null ? "//" : https + "//";
+    const httpStart = https == null ? "//" : (https ? "https://" : "http://");
     return `${httpStart}${hostWithoutPort}${port ? `:${port}` : ""}${path || ""}`;
 }
 
